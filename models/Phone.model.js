a3613fc0d62b15608f53e7bece4cbc05ccf10d15
Jose Luis <pepirob>
2023-03-30 16:58:18 +0200

    create phone model

--- models/Phone.model.js
+++ models/Phone.model.js
@@ -1,17 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-const phoneSchema = new Schema({
-  id: Number,
-  name: String,
-  manufacturer: String,
-  description: String,
-  color: String,
-  price: Number,
-  imageFileName: String,
-  screen: String,
-  processor: String,
-  ram: Number,
-});
+const phoneSchema = new Schema(
+  {
+    id: Number,
+    name: String,
+    manufacturer: String,
+    description: String,
+    color: String,
+    price: Number,
+    imageFileName: String,
+    screen: String,
+    processor: String,
+    ram: Number,
+  },
+  { timestamps: true }
+);
 
 const Phone = model("Phone", phoneSchema);
 
