3dfabd704906fa99877a37eabfc24024a34486f2
Jose Luis <pepirob>
2023-03-30 16:58:39 +0200

    create phone routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,4 +4,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+const phonesRouter = require("./phones.routes");
+router.use("/phones", phonesRouter);
+
 module.exports = router;
---
+++ routes/phones.routes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const Phones = require("../models/Phone.model");
+
+module.exports = router;
